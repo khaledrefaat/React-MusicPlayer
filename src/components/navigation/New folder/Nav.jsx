6260666a5e8afc314bdeefc04f18a0f49b9ff217
khaledrefaat <khaledrefaat08@gmail.com>
2022-01-12 23:44:34 +0200

    adding form-hook and validation file

---
+++ src/components/navigation/New folder/Nav.jsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import Header from './Header';
+import NavLinks from './NavLinks';
+
+import Toolbar from '@mui/material/Toolbar';
+import styled from '@emotion/styled';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { Link as RouterLink } from 'react-router-dom';
+
+const StyledToolbar = styled(Toolbar)`
+  display: flex;
+  justify-content: space-between;
+  position: relative;
+`;
+
+const MuiLink = styled(Link)`
+  color: #fff;
+  text-decoration: none;
+`;
+
+const Nav = () => {
+  return (
+    <Header>
+      {/* <StyledToolbar>
+        <Typography variant="h4">
+          <MuiLink component={RouterLink} to="/">
+            Blog
+          </MuiLink>
+        </Typography>
+        <NavLinks />
+      </StyledToolbar> */}
+    </Header>
+  );
+};
+
+export default Nav;
