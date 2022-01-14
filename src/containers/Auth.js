b9f2c2b5f7ab1c25e622ba71a1ceee0ac2217cf1
khaledrefaat <khaledrefaat08@gmail.com>
2022-01-14 08:05:00 +0200

    updating auth layout and it's responsive now

--- src/containers/Auth.js
+++ src/containers/Auth.js
@@ -5,59 +5,53 @@ import CustomButton from '../components/shared/CustomButton';
 import Input from '../components/shared/Input';
 
 import svg from '../assets/Alone.svg';
-
-import FileUpload from '../components/shared/FileUpload';
+import BackgroundPattern from '../components/shared/BackgroundPattern';
 
 const Auth = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   return (
-    <Container className="auth">
-      <div className="auth__card">
-        <div
-          style={{ backgroundImage: `url(${svg})` }}
-          className="auth__card--image"
-        />
-        <Form className="auth-form">
-          {!isLoginMode && (
-            <Input placeholder="name" id="name" name="name" type="text" />
-          )}
-          <Input placeholder="email" id="email" name="email" type="email" />
-          <Input
-            placeholder="password"
-            type="password"
-            name="password"
-            id="password"
-          />
-          {!isLoginMode && (
-            <>
+    <BackgroundPattern className="auth">
+      <div className="auth__card" style={{ backgroundImage: `url(${svg})` }}>
+        <div className="auth__form--container">
+          <Form className="auth-form">
+            {!isLoginMode && (
+              <Input placeholder="name" id="name" name="name" type="text" />
+            )}
+            <Input placeholder="email" id="email" name="email" type="email" />
+            <Input
+              placeholder="password"
+              type="password"
+              name="password"
+              id="password"
+            />
+            {!isLoginMode && (
               <Input
                 placeholder="password confirmation"
                 type="password"
                 name="passwordConfirmation"
                 id="passwordConfirmation"
               />
-              <FileUpload />
-            </>
-          )}
-          <CustomButton>Submit</CustomButton>
-          <div>
-            <span>
-              {isLoginMode
-                ? "Don't have an account?"
-                : 'Already have an account? '}
-            </span>
-            <CustomButton
-              type="button"
-              onClick={() => setIsLoginMode(isLoginMode => !isLoginMode)}
-              memberButton={true}
-            >
-              {isLoginMode ? 'SignUp' : 'Login'}
-            </CustomButton>
-          </div>
-        </Form>
+            )}
+            <CustomButton>Submit</CustomButton>
+            <div>
+              <span>
+                {isLoginMode
+                  ? "Don't have an account?"
+                  : 'Already have an account? '}
+              </span>
+              <CustomButton
+                type="button"
+                onClick={() => setIsLoginMode(isLoginMode => !isLoginMode)}
+                memberButton={true}
+              >
+                {isLoginMode ? 'SignUp' : 'Login'}
+              </CustomButton>
+            </div>
+          </Form>
+        </div>
       </div>
-    </Container>
+    </BackgroundPattern>
   );
 };
 
